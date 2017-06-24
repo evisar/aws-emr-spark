6432ecb9a8bc09b1debe24eade75b2e31c057728
Elmazi Visar <visar.elmazi@scania.com>
2017-06-24 04:58:28 +0200

    put conditions for key and bucket creation

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,15 +1,30 @@
 component_id=$(cat component.json | jq -r -c ".id")
+
 key_id=$(aws kms list-aliases | jq -r -c ".Aliases[] | select(.AliasName==\"alias/${component_id}\") | .TargetKeyId")
 if [ -z "$key_id" ] 
 then
   key_id=$(aws kms create-key | jq -r -c ".KeyMetadata.KeyId")
   aws kms create-alias --alias-name alias/${component_id} --target-key-id ${key_id}
 fi
-ssh_key_id=$(cat /proc/sys/kernel/random/uuid)
-aws ec2 create-key-pair --key-name ${ssh_key_id} | jq -r -c ".KeyMaterial">${ssh_key_id}.pem
-spark_cluster_id=$(aws emr create-cluster --name "${component_id}" --release-label emr-5.3.1 --instance-groups InstanceGroupType=MASTER,InstanceCount=1,InstanceType=m3.xlarge InstanceGroupType=CORE,InstanceCount=2,InstanceType=m3.xlarge --service-role EMR_DefaultRole --ec2-attributes KeyName=${ssh_key_id},InstanceProfile=EMR_EC2_DefaultRole --auto-scaling-role EMR_AutoScaling_DefaultRole  | jq -r -c ".ClusterId")
-aws kms encrypt --key-id ${key_id} --plaintext fileb://${ssh_key_id}.pem | jq -r -c ".CiphertextBlob" | base64 --decode>${spark_cluster_id}.pem
-aws s3api create-bucket --bucket ccp${component_id} --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
-aws s3 cp --sse aws:kms ${spark_cluster_id}.pem s3://ccp${component_id}/
-rm ${ssh_key_id}.pem
-rm ${spark_cluster_id}.*
+
+bucket_exists=$(aws s3api list-buckets | jq -r -c ".Buckets[] | select(.Name==\"ccp${component_id}\") | .Name")
+if [ -z "$bucket_exists" ] 
+then
+  aws s3api create-bucket --bucket ccp${component_id} --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
+fi
+
+key_pair_exists=$(aws ec2 describe-key-pairs | jq -r -c ".KeyPairs[] | select(.KeyName==\"${component_id}\") | .KeyName")
+if [ -z "$key_pair_exists" ] 
+then
+  aws ec2 create-key-pair --key-name ${component_id} | jq -r -c ".KeyMaterial">${component_id}.pem
+  aws kms encrypt --key-id ${key_id} --plaintext fileb://${component_id}.pem | jq -r -c ".CiphertextBlob" | base64 --decode>${component_id}.key
+  aws s3 cp --sse aws:kms ${component_id}.key s3://ccp${component_id}/
+  rm ${component_id}.*
+fi
+
+aws emr create-cluster --name "${component_id}" \
+  --release-label emr-5.3.1 \
+  --instance-groups InstanceGroupType=MASTER,InstanceCount=1,InstanceType=m3.xlarge InstanceGroupType=CORE,InstanceCount=2,InstanceType=m3.xlarge \
+  --service-role EMR_DefaultRole \
+  --ec2-attributes KeyName=${component_id},InstanceProfile=EMR_EC2_DefaultRole \
+  --auto-scaling-role EMR_AutoScaling_DefaultRole  
