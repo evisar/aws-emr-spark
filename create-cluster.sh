0d8b7c0448a8e8d1d048c35c7e44fd44d2260fd5
Elmazi Visar <visar.elmazi@scania.com>
2017-06-24 05:07:14 +0200

    reorder create-cluster arqgs

--- create-cluster.sh
+++ create-cluster.sh
@@ -7,10 +7,10 @@ then
   aws kms create-alias --alias-name alias/${component_id} --target-key-id ${key_id}
 fi
 
-bucket_exists=$(aws s3api list-buckets | jq -r -c ".Buckets[] | select(.Name==\"ccp${component_id}\") | .Name")
+bucket_exists=$(aws s3api list-buckets | jq -r -c ".Buckets[] | select(.Name==\"${component_id}\") | .Name")
 if [ -z "$bucket_exists" ] 
 then
-  aws s3api create-bucket --bucket ccp${component_id} --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
+  aws s3api create-bucket --bucket ${component_id} --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
 fi
 
 key_pair_exists=$(aws ec2 describe-key-pairs | jq -r -c ".KeyPairs[] | select(.KeyName==\"${component_id}\") | .KeyName")
@@ -18,7 +18,7 @@ if [ -z "$key_pair_exists" ]
 then
   aws ec2 create-key-pair --key-name ${component_id} | jq -r -c ".KeyMaterial">${component_id}.pem
   aws kms encrypt --key-id ${key_id} --plaintext fileb://${component_id}.pem | jq -r -c ".CiphertextBlob" | base64 --decode>${component_id}.key
-  aws s3 cp --sse aws:kms ${component_id}.key s3://ccp${component_id}/
+  aws s3 cp --sse aws:kms ${component_id}.key s3://${component_id}/
   rm ${component_id}.*
 fi
 
